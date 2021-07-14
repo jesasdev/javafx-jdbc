36f2b80cadfa8a0b6d6a7aa53123491d0598871c
Jesaias Santos <jesasdev@gmail.com>
2021-07-13 21:09:59 -0300

    Update Department

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -9,6 +9,7 @@ import application.Main;
 import gui.listeners.DatachangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -18,6 +19,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -27,9 +29,8 @@ import javafx.stage.Stage;
 import model.entities.Department;
 import model.service.DepartmentService;
 
-public class DepartmentListController implements Initializable,DatachangeListener {
+public class DepartmentListController implements Initializable, DatachangeListener {
 
-	
 	private DepartmentService service;
 	@FXML
 	private TableView<Department> tableViewDepartment;
@@ -39,6 +40,9 @@ public class DepartmentListController implements Initializable,DatachangeListene
 
 	@FXML
 	private TableColumn<Department, String> tableColunName;
+
+	@FXML
+	private TableColumn<Department, Department> tableColumnEDIT;
 
 	@FXML
 	private Button BtNew;
@@ -76,11 +80,13 @@ public class DepartmentListController implements Initializable,DatachangeListene
 	public void updateTableView() {
 		if (service == null) {
 			throw new IllegalStateException("Service was null");
+	
 		}
-		
+
 		List<Department> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartment.setItems(obsList);
+		initEditButtons();
 	}
 
 	private void createDialogForm(Department obj, String absolutName, Stage parentStage) {
@@ -93,11 +99,9 @@ public class DepartmentListController implements Initializable,DatachangeListene
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
 			controller.setDepartmentService(new DepartmentService());
-			//inscrevendo para receber o evento
+			// inscrevendo para receber o evento
 			controller.subscribeDataChangeListener(this);
 			controller.updateFormData();
-
-			
 
 			// funcao que carrega a janela do formulario pra prencher um novo departamento
 			Stage dialogStage = new Stage();
@@ -117,4 +121,22 @@ public class DepartmentListController implements Initializable,DatachangeListene
 		updateTableView();
 	}
 
+	private void initEditButtons() {
+		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEDIT.setCellFactory(param -> new TableCell<Department, Department>() {
+			private final Button button = new Button("edit");
+
+			@Override
+			protected void updateItem(Department obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(
+						event -> createDialogForm(obj, "/gui/DepartmentForm.fxml", Utils.currentStage(event)));
+			}
+		});
+	}
 }
